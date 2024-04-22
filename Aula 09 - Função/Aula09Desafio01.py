464ffcf0571a633e372b14dc6b545798b3d42db1
DERLEY4071 <anderleyfonseca71@gmail.com>
2024-04-22 16:05:51 -0300

    correçao exercicios

--- Aula 09 - Função/Aula09Desafio01.py
+++ Aula 09 - Função/Aula09Desafio01.py
@@ -3,15 +3,17 @@
 #Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
 
 def calculaArea(largura, comprimento):
-     area = largura * comprimento        
-     print(area)
+    area = largura * comprimento  
+    return area      
+    
+      
      
 largura = int(input("Digite a largura desejada :  "))
 comprimento = int(input("Digite a comprimento desejada :  "))
 
-print(f"O valor da area do terreno é :  ")
+print(f"O valor da area do terreno igual a {calculaArea(largura, comprimento)} m² : ")
 
-calculaArea(largura,comprimento)
+
 
 
 
---
+++ Aula 09 - Função/Aula09Desafio03.py
@@ -0,0 +1,22 @@
+# DESAFIO 03
+
+# Faça um programa que tenha uma função chamada maior(), que receba três parâmetros com valores inteiros.
+# Seu programa tem que analisar todos os valores e dizer qual deles é
+# o maior.
+
+n1 = int(input("Digite o primeiro valor : "))
+n2 = int(input("Digite o segundo valor : "))
+n3 = int(input("Digite o terceiro valor : "))
+
+def maior(numero1, numero2, numero3 ):
+    if numero1 > numero2 and numero1 > numero3:
+        return f" O numero {numero1} foi o maior valor"
+    
+    elif numero2 > numero3 :
+       return f" O numero {numero2} foi o maior valor"
+    else:
+         return f" O numero {numero3} foi o maior valor"
+
+print(maior(n1,n2,n3))
+        
+
