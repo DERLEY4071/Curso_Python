5792a68703793a100d96d6ef630c791afa9bbb48
DERLEY4071 <anderleyfonseca71@gmail.com>
2024-04-23 14:56:01 -0300

    correçao completa

--- Aula 10 - Tratamento Erros/aula10Atividade-Desafio03.py
+++ Aula 10 - Tratamento Erros/aula10Atividade-Desafio03.py
@@ -2,13 +2,15 @@
 
 # Escreva um programa que solicite ao usuário para digitar um número inteiro e exiba o resultado da sua raiz quadrada. Lide com o erro caso o número seja negativo.
 
+from math import sqrt
 try:
-    numero = int(input("Digite um numero inteiro : "))
+    numero = int(input("Digite um numero inteiro para achar a raiz quadrade : "))
     if numero <0:
             raise ValueError("O numero não pode ser negativo" )
-    raiz_quadrada = match.sqrt(numero)
-    print("A raiz quadrada de", numero,"é", raiz_quadrada)
+    raiz_quadrada = sqrt(numero)
+    print("A raiz quadrada de", numero,"é :", raiz_quadrada)
 except ValueError as ve:
     print(ve)
      
                             
+
--- Aula 10 - Tratamento Erros/aula10Atividade-Desafio04.py
+++ Aula 10 - Tratamento Erros/aula10Atividade-Desafio04.py
@@ -1,9 +1,5 @@
 # DESAFIO 04
-# 
-
-
-
-
+# Escreva um programa que peça ao usuário para digitar seu nome e idade. Em seguida, exiba uma mensagem personalizada que inclua o nome e a idade. Lide com o erro caso a idade digitada não seja um número.
 
 
 try:
@@ -20,5 +16,18 @@ except ValueError:
     mensagem_personalizada =  (f"Ola {nome}, você tem {idade} anos")
     print(mensagem_personalizada)
     
-        
     
+ # Correçao Pedro       
+    
+# try:
+    
+#     nome = input("Digite seu nome.: ")
+#     idade = input ("Digite sua idade.: ")
+        
+# except ValueError:
+#     print("Digite sua idade com valor numerico : ")
+    
+# else:
+#      print(f"Cadastro concluido")
+      
+   
